c988f686a87fcad5e9edb5c401fa5889367ad498
Pamcy <chinyi.pam@gmail.com>
2019-07-28 10:17:55 +0800

    :bento: Updating assets

---
+++ src/initial-playlist.js
@@ -0,0 +1,64 @@
+const albumInfo = {
+  tracks: [
+    {
+      id: 1,
+      title: 'Jazz Apricot',
+      featuring: 'Joey Pecoraro',
+      length: '1:52',
+    },
+    {
+      id: 2,
+      title: 'Write You',
+      featuring: 'Joey Pecoraro',
+      length: '2:26',
+    },
+    {
+      id: 3,
+      title: 'Fresh Healthy Perspectives',
+      featuring: 'Birocratic',
+      length: '2:41',
+    },
+    {
+      id: 4,
+      title: 'Good Job!',
+      featuring: 'R.LUM.R',
+      length: '2:51',
+    },
+    {
+      id: 5,
+      title: 'It Was a Fine Day',
+      featuring: 'South London HiFi',
+      length: '3:25',
+    },
+    {
+      id: 6,
+      title: 'Jazz Mango',
+      featuring: 'Joey Pecoraro',
+      length: '2:12',
+    },
+    {
+      id: 7,
+      title: 'Shredded aka Full Circle',
+      featuring: 'Noir Et Blanc Vie',
+      length: '3:11',
+    },
+    {
+      id: 8,
+      title: 'Unproductive Meetings',
+      featuring: 'Birocratic',
+      length: '1:40',
+    },
+    {
+      id: 9,
+      title: 'Bright Idea',
+      featuring: 'Geographer',
+      length: '2:40',
+    },
+    {
+      id: 10,
+      title: 'Church of 8 Wheels',
+      featuring: 'Otis McDonald',
+      length: '3:13',
+    },
+  ],
+};
